1a665370e68b7e9616de9fa1e63b4c4488070907
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:10:11 +1030

    feat: Created super constructor for Grain to Food

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -1,6 +1,10 @@
 package students.items;
 
-public class Grain{
+public class Grain extends Food{
+
+	public Grain(int maturationAge, int expectedLifespan, double monetaryValue) {
+		super(maturationAge, expectedLifespan, monetaryValue);
+	}
 
 	
 }
