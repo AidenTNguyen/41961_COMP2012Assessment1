00cd48576e487871394513f5cf938842c0b9adc6
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:21:00 +1030

    feat/refactor: added toString for grain and changed super constructor
    added toString to represent level of maturity for grain and refactored constructor for predetermined values for Grain

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -2,9 +2,24 @@ package students.items;
 
 public class Grain extends Food{
 
-	public Grain(int maturationAge, int expectedLifespan, double monetaryValue) {
-		super(maturationAge, expectedLifespan, monetaryValue);
+	/**
+	 * Subclass Grain inherits from Food extending from Item
+	 */
+	public Grain() {
+		super(2, 6, 2.0); //Grain has predetermiend values of maturity, expected lifespan and monetary value
 	}
-
+	
+	/**
+	 * Overrides toString method to return representation of grain depending on level of maturity
+	 */
+	@Override
+	public String toString() {
+		if (age < maturationAge) {
+			return "g"; //representation for grain below maturity
+		} else {
+			return "G"; //representation for grain that has reached maturity
+		}
+	}
+	
 	
 }
