79b1cb872e573618bcc9a15014095128a992dd3e
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:06:47 +1030

    fix: added super constructor for Food to extend Item

--- src/students/items/Food.java
+++ src/students/items/Food.java
@@ -1,5 +1,12 @@
 package students.items;
 
 public abstract class Food extends Item{
+
+	/**
+	 * super class of Food extending from Item using the super method to copy inherit Item parameters
+	 */
+	public Food(int maturationAge, int expectedLifespan, double monetaryValue) {
+		super(maturationAge, expectedLifespan, monetaryValue);
+	}
 	
 }
