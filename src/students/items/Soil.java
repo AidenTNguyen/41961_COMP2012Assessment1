c0e4cb7753ff15ab04530b581b25d64ae8176bc6
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:47:49 +1030

    feat: implemented Soil class extending from Food
    almost identical implementation from UntilledSoil class other than value being 0 and being represented as "."

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -1,5 +1,21 @@
 package students.items;
 
-public class Soil {
+public class Soil extends Food{
+	
+	/**
+	 * Super constructor for the class Soil with predetermined values of -1 for maturation and death age as well as value
+	 */
+	public Soil() {
+		super(-1, -1, 0); //As a placeholder for infinity -1 will be used for both maturation age and death age
+	}
+	
+	/**
+	 * Overriden toString method to represent Soil class
+	 */
+	@Override
+	public String toString() {
+		return ".";
+	}
 
 }
+
