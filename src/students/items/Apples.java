06cdcb88d2ff4ec937dce4be40e317f16c1f25b5
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:34:52 +1030

    feat: reused Grain code for Apples class
    Duplicated code from Grain class onto Apples class and changed the names and predetermined superconstructor parameters

--- src/students/items/Apples.java
+++ src/students/items/Apples.java
@@ -1,5 +1,37 @@
 package students.items;
 
-public class Apples {
-
+public class Apples extends Food {
+	
+	private static final double seedCost = 2.0; //Cost of grain seed to buy before being grown
+	private static int instanceCount = 0; //Count of number of apple objects
+	
+	/**
+	 * Subclass Apple inherits from Food extending from Item
+	 */
+	public Apples() {
+		super(3, 5, 3.0); //Apple has predetermiend values of maturity, expected lifespan and monetary value
+		instanceCount++; //Increments the count of apple instances
+	}
+	
+	/**
+	 * Overrides toString method to return representation of apple depending on level of maturity
+	 */
+	@Override
+	public String toString() {
+		if (age < maturationAge) {
+			return "a"; //representation for apples below maturity
+		} else {
+			return "A"; //representation for apple that has reached maturity
+		}
+	}
+	
+	/**
+	 * Method to return the number of instantiated apples as a string
+	 * @return number of instantiated apple instances
+	 */
+	public String getGenerationCount() {
+		return "Number of instantiated apple objects: " + instanceCount;
+	}
+	
+	
 }
