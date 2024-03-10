9b5b8bbc5595b1bf3952aeafa70029ac6276f6f9
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-10 23:14:17 +1030

    Feat. Item class constructor
    Established constructor with age, maturationAge, deathAge and monetaryValue variables

--- src/students/items/Item.java
+++ src/students/items/Item.java
@@ -1,6 +1,30 @@
 package students.items;
 
-public class Item {
+/**
+ * Represents the base class of all foods.
+ */
 
+public class Item {
+	
+	//Variables to be used in constructing of Item objects
+	private int age = 0;
+	private int maturationAge = 0;
+	private int deathAge = 0;
+	private double monetaryValue = 0.0;
+	
+	/**
+	 * Constructs a new Item object with age of maturity, death and monetary value
+	 * @param age represents the current age of the item (starts at 0)
+	 * @param maturationAge age of which the item reaches maturity
+	 * @param deathAge the expected lifespan of the item
+	 * @param monetaryValue the value of the item
+	 */
+	public Item(int age, int maturationAge, int deathAge, double monetaryValue) {
+		
+		this.age = age;
+		this.maturationAge = maturationAge;
+		this.deathAge = deathAge;
+		this.monetaryValue = monetaryValue;
+	}
 	
 }
