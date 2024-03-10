926e90164f0ba4a338e49defb750d15890cad084
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:30:28 +1030

    feat: added getGenerationCount() and associated class declarations
    implemented getGenerationCount method to return the number of instances of grain objects.

--- src/students/items/Grain.java
+++ src/students/items/Grain.java
@@ -3,12 +3,14 @@ package students.items;
 public class Grain extends Food{
 
 	private static final double seedCost = 1.0; //Cost of grain seed to buy before being grown
+	private static int instanceCount = 0; //Count of number of grain objects
 	
 	/**
 	 * Subclass Grain inherits from Food extending from Item
 	 */
 	public Grain() {
 		super(2, 6, 2.0); //Grain has predetermiend values of maturity, expected lifespan and monetary value
+		instanceCount++; //Increments the count of grain instances
 	}
 	
 	/**
@@ -21,6 +23,14 @@ public class Grain extends Food{
 		} else {
 			return "G"; //representation for grain that has reached maturity
 		}
+	}
+	
+	/**
+	 * Method to return the number of instantiated grains as a string
+	 * @return number of instantiated grain instances
+	 */
+	public String getGenerationCount() {
+		return "Number of instantiated grain objects: " + instanceCount;
 	}
 	
 	
