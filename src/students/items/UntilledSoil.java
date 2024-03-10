714b4eb64c7ff5e60ff93fad41cf7edfdc21d902
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:45:44 +1030

    feat: Implemented UntilledSoil class extending from Food
    Implemented almost exactly identically as Weed with -1 values to represent infinity in maturation and death age parameters along with a toString method to represent UntilledSoil as "/"

--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -1,6 +1,21 @@
 package students.items;
 
-public class UntilledSoil {
+public class UntilledSoil extends Food {
 
+	/**
+	 * Super constructor for the class UntilledSoil with predetermined values of -1 for maturation and death age as well as value
+	 */
+	public UntilledSoil() {
+		super(-1, -1, -1); //As a placeholder for infinity -1 will be used for both maturation age and death age
+	}
+	
+	/**
+	 * Overriden toString method to represent UntilledSoil class
+	 */
+	@Override
+	public String toString() {
+		return "/";
+	}
 
 }
+
