7c410540643bd520aa9bdc760c64644d572b60c7
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-11 01:42:36 +1030

    feat: Created subclass Weed from Food
    Used -1 to represent infinity for death and maturation age in the super constructor and implemented a toString method to represent weed as "#"

--- src/students/items/Weed.java
+++ src/students/items/Weed.java
@@ -1,6 +1,20 @@
 package students.items;
 
-public class Weed
-{
+public class Weed extends Food {
+
+	/**
+	 * Super constructor for the class Weed with predetermined values of -1 for maturation and death age as well as value
+	 */
+	public Weed() {
+		super(-1, -1, -1); //As a placeholder for infinity -1 will be used for both maturation age and death age
+	}
+	
+	/**
+	 * Overriden toString method to represent Weed class
+	 */
+	@Override
+	public String toString() {
+		return "#";
+	}
 
 }
