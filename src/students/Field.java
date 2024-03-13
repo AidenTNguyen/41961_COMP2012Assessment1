528ad51f730ad7968d8577f41e538cef42679592
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-14 08:58:57 +1030

    feat: created constructor for field class

--- src/students/Field.java
+++ src/students/Field.java
@@ -3,8 +3,15 @@ package students;
 
 public class Field {
 	
-	public Field(int height, int width)
-	{
+	//Class variables for height and width of field
+	private static int height = 0;
+	private static int width = 0;
+	
+	public Field(int height, int width) {
+
+		this.height = height;
+		this.width = width;
+		
 	}
 	
 }
