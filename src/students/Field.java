2e5bb49f132d9f813ec9aa424e45e62452cd49fa
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-14 09:21:09 +1030

    feat: adding logic to field constructor
    Trying to fill the field with soil on object construction

--- src/students/Field.java
+++ src/students/Field.java
@@ -1,5 +1,5 @@
 package students;
-import students.items.Item;
+import students.items.Item; //Importing Item class from student.items package to use in a 2D array
 
 
 public class Field {
@@ -7,18 +7,24 @@ public class Field {
 	//Class variables for height and width of field
 	private static int height = 0;
 	private static int width = 0;
-	private static Item[][] fieldDimensions; //This value holds a 2D array of height and width
+	private static Item[][] fieldDimensions; //This value holds a 2D array of objects
 	
 	public Field(int height, int width) {
 
 		this.height = height;
 		this.width = width;
-		fieldDimensions = new int[height][width];
+		fieldDimensions = new Item[height][width];
+		
+		//Logic to fill the dimensions of the Field object with soil
+		for (Item[] rowIndex : fieldDimensions) { //Outer loop to iterate over array elements of 2D array
+			for (Item columnIndex : rowIndex) //Inner loop to iterate over int values on rowIndex arrays
 	}
+		
+		
 	
 	public void tick() {
-		for (int[] rowIndex : fieldDimensions) { //Outer loop to iterate over array elements of 2D array
-			for (int columnIndex : rowIndex) { //Inner loop to iterate over int values on rowIndex arrays
+		for (int[] rowIndex : fieldDimensions) {
+			for (int columnIndex : rowIndex) {
 				columnIndex.tick();
 				
 			}
