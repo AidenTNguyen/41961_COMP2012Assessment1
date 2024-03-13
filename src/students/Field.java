d68323df8782b821846e5d878694493b807a2e8a
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-14 09:04:29 +1030

    feat: added a 2D array class variable
    This is to be used when printing or iterating over each item in the field (hopefully)

--- src/students/Field.java
+++ src/students/Field.java
@@ -6,11 +6,13 @@ public class Field {
 	//Class variables for height and width of field
 	private static int height = 0;
 	private static int width = 0;
+	private static int[][] fieldDimensions; //This value holds a 2D array of height and width
 	
 	public Field(int height, int width) {
 
 		this.height = height;
 		this.width = width;
+		fieldDimensions = new int[height][width];
 		
 	}
 	
