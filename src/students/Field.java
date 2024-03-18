2a5cc6b6490749626b4b5a9e846224567fe0ba41
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-18 18:08:17 +1030

    fix: changed index value back to 0
    instead of setting it at 1 which would cause the iteration through 2D arrays to go out of bounds we add 1 to the value of the index's added on top of result

--- src/students/Field.java
+++ src/students/Field.java
@@ -65,22 +65,21 @@ public class Field {
 	public String toString() {
 		String result = "";//placeholder string to hold the final String representation of the grid
 		
-		for (int columnIndex = 0; columnIndex < width; columnIndex++) {
-			result += columnIndex + " "; //y-axis numbers			
+		for (int columnIndex = 0; columnIndex <= width; columnIndex++) {
+			result += (columnIndex + 1) + " "; //y-axis numbers			
 		}
 		result += "\n";
 		
+		for (int rowIndex = 0; rowIndex <= height; rowIndex++) {
+			result += (rowIndex + 1) + "\n"; //x-axis number
+			
+			for (int columnIndex = 0)
+				
+			}
+		}
 
 		
 	}
 	
-//	public String toString() {
-//		Soil freshSoil = new Soil();
-//		return for (Item[] rowIndex : fieldDimensions) {
-//			for (Item columnIndex : rowIndex) {
-//				freshSoil.toString();
-//			}
-//		}
-//	}
 	
 }
