ce18a94b2c88b11cf654a4bddfaf151633115339
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-18 19:23:54 +1030

    refactor: changed monetaryValue type of derived classes
    chagned values from -1 to -1.0 to reflect the Double type of the parameter

--- src/students/items/Soil.java
+++ src/students/items/Soil.java
@@ -6,7 +6,7 @@ public class Soil extends Food{
 	 * Super constructor for the class Soil with predetermined values of -1 for maturation and death age as well as value
 	 */
 	public Soil() {
-		super(-1, -1, 0); //As a placeholder for infinity -1 will be used for both maturation age and death age
+		super(-1, -1, 0.0); //As a placeholder for infinity -1 will be used for both maturation age and death age
 	}
 	
 	/**
--- src/students/items/UntilledSoil.java
+++ src/students/items/UntilledSoil.java
@@ -6,7 +6,7 @@ public class UntilledSoil extends Food {
 	 * Super constructor for the class UntilledSoil with predetermined values of -1 for maturation and death age as well as value
 	 */
 	public UntilledSoil() {
-		super(-1, -1, -1); //As a placeholder for infinity -1 will be used for both maturation age and death age
+		super(-1, -1, -1.0); //As a placeholder for infinity -1 will be used for both maturation age and death age
 	}
 	
 	/**
