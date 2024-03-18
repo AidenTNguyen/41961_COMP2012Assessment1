6d7f23869acea207f78d28538680b8da64c1e293
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-19 00:58:34 +1030

    small fix to the language

--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -53,7 +53,7 @@ public class Marker {
 
 		// Section THREE
 		// uncomment this section once you get to it
-		//remember, your farm should work on varies size fields (assuming all fields are less than 10x10)
+		//remember, your farm should work on varies size fields (assuming all fields are less than or equal to 10x10)
 		//Farm f = new Farm(10, 5, 10);
 		//f.run();
 		
