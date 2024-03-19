7cf5d181d4ca1603fcec204c91243df3d473a676
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-19 20:43:56 +1030

    feat: added Farm constructor parameters

--- src/students/Farm.java
+++ src/students/Farm.java
@@ -2,8 +2,15 @@ package students;
 
 public class Farm {
 	
-	public Farm(int fieldWidth, int fieldHeight, double startingFunds)
-	{
+	private int fieldWidth = 0;
+	private int fieldHeight = 0;
+	private double startingFunds = 0.0;
+	
+	public Farm(int fieldWidth, int fieldHeight, double startingFunds) {
+		
+		this.fieldWidth = fieldWidth;
+		this.fieldHeight = fieldHeight;
+		this.startingFunds = startingFunds;
 	}
 	
 	public void run()
