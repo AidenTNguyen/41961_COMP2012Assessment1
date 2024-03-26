33d7ae87202acd31d53518fe1929a68d3ab9f8b1
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-27 04:02:11 +1030

    feat: Added two new classes
    These two classes will be the focal point of the new feature

---
+++ src/students/BoonList.java
@@ -0,0 +1,6 @@
+package students;
+
+// This class will contain a number of boons to affect SimFarm gameplay
+public class BoonList {
+
+}
---
+++ src/students/GameController.java
@@ -0,0 +1,6 @@
+package students;
+
+// This class will control and regulate any boons or modifiers added to the game
+public class GameController {
+
+}
