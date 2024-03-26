fb8d1131fdff2ba5245f1f8856a50fabaf147304
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-27 04:39:05 +1030

    feat: implemented getWinCost method
    returns a copy of the win cost to the Farm class to be used as a game condition

--- src/students/GameController.java
+++ src/students/GameController.java
@@ -2,16 +2,34 @@ package students;
 
 // This class will control and regulate any boons or modifiers added to the game
 public class GameController {
-	private static double winCost = 0.0;
+	
+	private static double winCost = 25.0;
+	
 	
 	
 	/**
-	 * This method calculated the growth of the win condition
+	 * This method will return the cost to win the game
+	 * @return winCost
+	 */
+	public static double getWinCost() {
+		
+		double initialGrowthRate = 1.5; // This may be changed with a difficulty level system
+		
+		double newWinCost = calculateWinCost(winCost, initialGrowthRate);
+		winCost = newWinCost;
+		
+		return newWinCost;
+	}
+	
+	
+	
+	/**
+	 * This method calculates the growth of the win condition
 	 * @param initialAmount is the initial win cost
 	 * @param growthRate how quickly the
 	 * @return the new cost to win the round
 	 */
-	public static double calculateWinCost(double initialAmount, int growthRate) {
+	private static double calculateWinCost(double initialAmount, double growthRate) {
 		
 		double calculatedCost = initialAmount * (1 + growthRate);
 		return calculatedCost;
