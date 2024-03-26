93f760a4d6f5f19b682baaa6675b574667bae2fd
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-27 04:07:19 +1030

    feat: added two boon types to the list

--- src/students/BoonList.java
+++ src/students/BoonList.java
@@ -3,4 +3,21 @@ package students;
 // This class will contain a number of boons to affect SimFarm gameplay
 public class BoonList {
 
+	/**
+	 * This class will modify the value of the Apple object
+	 * @param percentage a double that will boost the value of Apple objects
+	 * @return the new value of the apple object
+	 */
+//	public static double appleModifier(double percentage) {
+//		
+//	}
+	
+	/**
+	 * This class will modify the value of the Grain object
+	 * @param percentage a double that will bost the value of Grain objects
+	 * @return the new value of the grain object
+	 */
+//	public static double grainModifier(double percentage) {
+//		
+//	}
 }
