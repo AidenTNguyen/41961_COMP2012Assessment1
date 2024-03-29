bfcb86bcc292872d69c227a6e3397e18dedb05c6
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-29 21:24:22 +1030

    feat: added the getModifierPercentage to the superclass

--- src/students/ItemModifiers.java
+++ src/students/ItemModifiers.java
@@ -2,6 +2,8 @@ package students;
 
 public abstract class ItemModifiers {
 	
+	double generatedBuff = 0.0; // Buff percentage in the range of minRange to maxRange
+	
 	/**
 	 * Generates a random number between minRange (inclusive) and maxRange(exclusive)
 	 * @param minRange
@@ -12,6 +14,13 @@ public abstract class ItemModifiers {
 		double initialRandomNum = Math.random();
 		double randomPercentage = (initialRandomNum * (maxRange - minRange)) + minRange; // Turns the initialRandomNum to a number between the ranges
 		return Math.floor(randomPercentage * 100) / 100; // Rounds that randomPercentage to 2 decimal points
+	}
+	
+	/**
+	 * @return returns a copy of the generated buff percentage
+	 */
+	protected double getModifierPercentage() {
+		return generatedBuff;
 	}
 
 }
