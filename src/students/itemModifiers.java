8adb3e72bfe9c469c3472663e6782e801e9bd16a
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-29 21:04:32 +1030

    chore: added documentation

--- src/students/itemModifiers.java
+++ src/students/itemModifiers.java
@@ -2,8 +2,15 @@ package students;
 
 public abstract class itemModifiers {
 	
+	/**
+	 * Generates a random number between minRange (inclusive) and maxRange(exclusive)
+	 * @param minRange
+	 * @param maxRange
+	 * @return final double value
+	 */
 	private double getRandomPercentage(double minRange, int maxRange) {
 		double initialRandomNum = Math.random();
+		
 	}
 
 }
