52238f4eb2f52afe9ba4ee9e826539b289763dee
Aiden Nguyen <nguty385@mymail.unisa.edu.au>
2024-03-29 21:25:30 +1030

    feat: implemented the GrainBuff object

--- src/students/buffs/GrainBuff.java
+++ src/students/buffs/GrainBuff.java
@@ -4,4 +4,10 @@ import students.ItemModifiers;
 
 public class GrainBuff extends ItemModifiers {
 
+	public GrainBuff(double minRange, double maxRange) {
+		this.generatedBuff = getRandomPercentage(minRange, maxRange);
+		
+	}
+	
+	
 }
